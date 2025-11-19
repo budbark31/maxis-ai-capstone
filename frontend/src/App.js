f877fbe723526f9cba08fad6dc6189ddd9e79719
Budbark31 <bivinsshilo@gmail.com>
2025-11-18 21:33:50 -0500

    update 8001 to 8000 in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ const SourceIcon = () => (
   </svg>
 );
 
-const API_BASE = process.env.REACT_APP_API_BASE_URL || "http://localhost:8001";
+const API_BASE = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
 
 export default function App() {
   const [messages, setMessages] = useState([]);
